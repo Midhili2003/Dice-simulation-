8f2190203d056608a1312eab19df57313ba68971
Midhili2003 <83817099+Midhili2003@users.noreply.github.com>
2021-05-08 21:36:23 +0530

    Update dice simulator.py

--- dice simulator.py
+++ dice simulator.py
@@ -3,28 +3,28 @@ a = 'y'
 while a == 'y':
     no = random.randint(1 , 6)
     if no == 1:
-        print("[          ]")
-        print("[    o   ]")
-        print("[          ]")
+        print("[     ]")
+        print("[  o  ]")
+        print("[     ]")
     if no == 2:
-        print("[o       ]")
-        print("[          ]")
-        print("[       o]")
+        print("[o    ]")
+        print("[     ]")
+        print("[    o]")
     if no == 3:
-        print("[          ]")
+        print("[     ]")
         print("[o o o]")
-        print("[          ]")
+        print("[     ]")
     if no == 4:
-        print("[o     o]")
-        print("[          ]")
-        print("[o     o]")
+        print("[o   o]")
+        print("[     ]")
+        print("[o   o]")
     if no == 5:
-        print("[o     o]")
-        print("[    o   ]")
-        print("[o     o]")
+        print("[o   o]")
+        print("[  o  ]")
+        print("[o   o]")
     if no == 6:
         print("[o o o]")
-        print("[          ]")
+        print("[     ]")
         print("[o o o]")
 
     a = input("press y to roll again and x to exit:")
